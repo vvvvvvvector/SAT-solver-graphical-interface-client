71ff7a0d0d089e70dc881c9aac5decd5de23ee4e
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-01-31 00:46:01 +0100

    fix: now user sees only parsed formula and has container with the answers +other small changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,19 @@ type SolveType = {
   satisfiable: boolean;
 } | null;
 
-type NextType = {
-  clause: {
-    id: number;
-    variables: number[];
-  };
-  satisfiable: boolean;
-} | null;
+type AnswerType = {
+  id: number;
+  variables: number[];
+};
 
 export default function App() {
   const [formula, setFormula] = React.useState(""); // textArea
 
   const [solveResponse, setSolverResponse] = React.useState<SolveType>(null); // formula
-  const [nextResponse, setNextResponse] = React.useState<NextType>(null); // formula
 
-  const [clauses, setClauses] = React.useState([]);
+  const [answers, setAnswers] = React.useState<AnswerType[]>([]);
+
+  // console.log(answers);
 
   return (
     <>
@@ -38,13 +36,35 @@ export default function App() {
         <FormulaContext.Provider value={{ formula, setFormula }}>
           <FormulaArea />
           <Controls
-            setClauses={setClauses}
+            setAnswers={setAnswers}
             setSolveResponse={setSolverResponse}
-            setNextResponse={setNextResponse}
           />
         </FormulaContext.Provider>
-        {solveResponse && <Formula clauses={clauses} />}
-        {nextResponse?.clause.variables.map((i, index) => (
+        {solveResponse && <Formula clauses={solveResponse.clauses} />}
+
+        <div className="container">
+          {answers.map((clause, clauseIndex) => (
+            <div className="answers" key={clauseIndex}>
+              {clause.variables.map((i, variableIndex) => (
+                <div className="answer" key={variableIndex}>
+                  <Variable
+                    variable={{
+                      id: variableIndex,
+                      index: Math.abs(i),
+                      clauseId: clause.id,
+                    }}
+                  />
+                  <span className="equals">=</span>
+                  <span className={i > 0 ? "red" : "green"}>
+                    {i > 0 ? "FALSE" : "TRUE"}
+                  </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        {/* {nextResponse?.clause.variables.map((i, index) => (
           <div className="answer" key={index}>
             <Variable
               variable={{
@@ -58,7 +78,7 @@ export default function App() {
               {i > 0 ? "FALSE" : "TRUE"}
             </span>
           </div>
-        ))}
+        ))} */}
       </div>
     </>
   );
