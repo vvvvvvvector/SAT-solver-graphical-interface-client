ff4af28dbf3e370bc0b562876eea949313eb2739
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-07 00:02:21 +0100

    now user can delete variables and clauses from parsed formula

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
+import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
 import styles from "./Solutions.module.scss";
