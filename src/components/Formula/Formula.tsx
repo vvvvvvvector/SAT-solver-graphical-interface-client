990c8c0fcd1e3e0e335a00aa3055a6b7947d3d2f
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-07 11:40:20 +0100

    no formula, no solutions divs

--- src/components/Formula/Formula.tsx
+++ src/components/Formula/Formula.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useSelector } from "react-redux";
+import solutions from "../../redux/slices/solutions";
 import { RootState } from "../../redux/store";
 
 import { Clause } from "../index";
@@ -14,37 +15,43 @@ export const Formula: React.FC = () => {
 
   return (
     <>
-      {clauses.length > 0 && (
-        <>
-          <div className={styles.header}>
-            <h2 onClick={() => setOpened(!opened)}>Formula in CNF</h2>
-            <svg
-              className={opened ? styles["active"] : ""}
-              width="10"
-              height="6"
-              viewBox="0 0 10 6"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                fill="#2C2C2C"
-              />
-            </svg>
-          </div>
-          {opened && (
-            <div className={styles["formula-container"]}>
-              <ul className={styles.formula}>
-                {clauses.map((clause, index) => (
-                  <li key={clause.id}>
-                    <Clause clause={clause} />
-                    {clauses.length - 1 > index && <span>&#8743;</span>}
-                  </li>
-                ))}
-              </ul>
+      <div className={styles.header}>
+        <h2 onClick={() => setOpened(!opened)}>
+          {clauses.length > 0
+            ? "Formula in CNF"
+            : "There are no formula so far"}
+        </h2>
+        <svg
+          className={opened ? styles["active"] : ""}
+          width="10"
+          height="6"
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+            fill="#2C2C2C"
+          />
+        </svg>
+      </div>
+      {opened && (
+        <div className={styles["formula-container"]}>
+          {clauses.length > 0 ? (
+            <ul className={styles.formula}>
+              {clauses.map((clause, index) => (
+                <li key={clause.id}>
+                  <Clause clause={clause} />
+                  {clauses.length - 1 > index && <span>&#8743;</span>}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className={styles["no-formula"]}>
+              <span>😭😭😭</span>
             </div>
           )}
-        </>
+        </div>
       )}
     </>
   );
