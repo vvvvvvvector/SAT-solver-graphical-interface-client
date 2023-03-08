5a516927bf7b7ffa30897c7f6e3b53221c9240fa
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-09 00:47:53 +0100

    new icons for delete and edit buttons; there are <input/> instead of prompt on edit; other small changes

--- src/components/Clause/Clause.tsx
+++ src/components/Clause/Clause.tsx
@@ -4,6 +4,8 @@ import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { editClause, removeClause } from "../../redux/slices/formula";
 
+import SaveIcon from "@mui/icons-material/Save";
+
 import { Variable } from "../index";
 
 import { ClauseType } from "../../shared/types";
@@ -13,50 +15,85 @@ import styles from "./Clause.module.scss";
 export const Clause: React.FC<{ clause: ClauseType }> = ({ clause }) => {
   const dispatch = useDispatch();
 
+  const [editMode, setEditMode] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
   const onRemoveClause = () => {
     dispatch(removeClause(clause.id));
+
+    toast.success("Clause was successfully removed!");
 
     sessionStorage.clear();
   };
 
   const onEditClause = () => {
-    let input = window.prompt("Edit clause: ", clause.variables.join("|"));
+    dispatch(
+      editClause({
+        id: clause.id,
+        editedClause: value.split("|").map((i) => parseInt(i)),
+      })
+    );
 
-    let editedClause = input?.split("|").map((item) => parseInt(item));
+    setEditMode(false);
 
-    if (editedClause) {
-      dispatch(editClause({ id: clause.id, editedClause }));
-      toast.success("Clause was successfully updated!");
-    }
+    toast.success("Clause was successfully updated!");
 
     sessionStorage.clear();
   };
 
   return (
     <div className={styles.container}>
-      <ul onClick={onEditClause} className={styles.clause}>
-        <p>(</p>
-        {clause.variables.map((i, index) => (
-          <li key={index}>
-            <Variable variable={{ id: index, index: i, clauseId: clause.id }} />
-            {clause.variables.length - 1 > index && <span>&#8744;</span>}
-          </li>
-        ))}
-        <p>)</p>
-      </ul>
-      <svg
-        onClick={onRemoveClause}
-        width="13"
-        height="13"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M6.24741 5L9.73899 1.50842C9.9047 1.343 9.99791 1.11853 9.99812 0.884393C9.99832 0.650251 9.90551 0.425617 9.74009 0.259907C9.57468 0.0941973 9.35021 0.000986589 9.11606 0.000779811C8.88192 0.000573033 8.65729 0.0933872 8.49158 0.258804L5 3.75038L1.50842 0.258804C1.34271 0.0930948 1.11796 0 0.883613 0C0.649264 0 0.424514 0.0930948 0.258804 0.258804C0.0930948 0.424514 0 0.649264 0 0.883613C0 1.11796 0.0930948 1.34271 0.258804 1.50842L3.75038 5L0.258804 8.49158C0.0930948 8.65729 0 8.88204 0 9.11639C0 9.35074 0.0930948 9.57549 0.258804 9.7412C0.424514 9.90691 0.649264 10 0.883613 10C1.11796 10 1.34271 9.90691 1.50842 9.7412L5 6.24962L8.49158 9.7412C8.65729 9.90691 8.88204 10 9.11639 10C9.35074 10 9.57549 9.90691 9.7412 9.7412C9.90691 9.57549 10 9.35074 10 9.11639C10 8.88204 9.90691 8.65729 9.7412 8.49158L6.24741 5Z"
-          fill="#E3E3E3"
-        />
-      </svg>
+      {editMode ? (
+        <div className={styles.edit}>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          <SaveIcon
+            onClick={onEditClause}
+            fontSize="small"
+            className={styles.save}
+          />
+        </div>
+      ) : (
+        <>
+          <ul className={styles.clause}>
+            <p>(</p>
+            {clause.variables.map((i, index) => (
+              <li key={index}>
+                <Variable
+                  variable={{ id: index, index: i, clauseId: clause.id }}
+                />
+                {clause.variables.length - 1 > index && <span>&#8744;</span>}
+              </li>
+            ))}
+            <p>)</p>
+          </ul>
+          <div className={styles.controls}>
+            <svg
+              onClick={() => {
+                setEditMode(true);
+                setValue(clause.variables.join("|"));
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 30 30"
+              width="17px"
+              height="17px"
+            >
+              <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z" />
+            </svg>
+            <svg
+              onClick={onRemoveClause}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="17px"
+              height="17px"
+            >
+              <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z" />
+            </svg>
+          </div>
+        </>
+      )}
     </div>
   );
 };
