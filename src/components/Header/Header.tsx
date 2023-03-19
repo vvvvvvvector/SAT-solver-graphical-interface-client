a97e31b245e156d2725b3fef36724dbd58564873
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-19 02:08:22 +0100

    react-router-dom connected

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 
+import { Outlet, Link } from "react-router-dom";
+
 import styles from "./Header.module.scss";
 
 export const Header: React.FC = () => {
   return (
-    <header className={styles.header}>
-      <div>
-        <h2>
-          SAT-solver graphical interface<span>🤖</span>
-        </h2>
-        <ul>
-          <li>Home</li>
-          <li>Formulas linker</li>
-        </ul>
-      </div>
-    </header>
+    <>
+      <header className={styles.header}>
+        <div>
+          <h2>
+            SAT-solver graphical interface<span>🤖</span>
+          </h2>
+          <ul>
+            <li>
+              <Link
+                style={{ color: "inherit", textDecoration: "inherit" }}
+                to={"/"}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ color: "inherit", textDecoration: "inherit" }}
+                to={"/linker"}
+              >
+                Formulas linker
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 };
