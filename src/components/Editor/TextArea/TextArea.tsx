77d4fc7dcfce6c918a5546229bad4660d0335cd5
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-19 15:55:50 +0100

    refactoring + handling error when formula is already defined

--- src/components/Editor/TextArea/TextArea.tsx
+++ src/components/Editor/TextArea/TextArea.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { removeError, setDimacs, addError } from "../../../redux/slices/editor";
+import { setDimacs } from "../../../redux/slices/editor";
 import { RootState } from "../../../redux/store";
-
-import debounce from "lodash.debounce";
 
 import styles from "./TextArea.module.scss";
 
@@ -16,86 +14,19 @@ const TextArea: React.FC<{
 
   const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
 
-  const { dimacs, errors } = useSelector((state: RootState) => state.editor);
-
-  const verify = React.useCallback(
-    debounce((dimacs: string) => {
-      const lineByLineDimacs = dimacs.split("\n");
-
-      const formulaDefinition = /^p\scnf\s[1-9][0-9]*\s[1-9][0-9]*$/;
-      const lineEndsWithZero = / 0$/;
-      const validClause = /^(?:-?[1-9][0-9]*\s)+0$/;
-
-      lineByLineDimacs.forEach((line, index) => {
-        if (line.startsWith("c")) return;
-
-        const error = errors.find((error) => error.line === index + 1);
-
-        if (line === "" && index !== lineByLineDimacs.length - 1) {
-          if (!error)
-            dispatch(
-              addError({
-                line: index + 1,
-                text: "err: empty line",
-              })
-            );
-        } else if (!line.match(formulaDefinition) && line.startsWith("p")) {
-          if (!error)
-            dispatch(
-              addError({
-                line: index + 1,
-                text: "err: invalid formula definition",
-              })
-            );
-        } else if (
-          !line.match(lineEndsWithZero) &&
-          line !== "" &&
-          !line.startsWith("p")
-        ) {
-          if (!error)
-            dispatch(
-              addError({
-                line: index + 1,
-                text: "err: clause must end with 0",
-              })
-            );
-        } else if (
-          !line.match(validClause) &&
-          line !== "" &&
-          !line.startsWith("p")
-        ) {
-          if (!error)
-            dispatch(
-              addError({
-                line: index + 1,
-                text: "err: invalid clause",
-              })
-            );
-        } else {
-          if (error) dispatch(removeError(index));
-        }
-      });
-    }, 650),
-    []
-  );
-
-  React.useEffect(() => {
-    verify(dimacs);
-  }, [dimacs]);
-
-  const handleScroll = React.useCallback(() => {
-    if (gutterRef.current && textAreaRef.current && errorsRef.current) {
-      const { scrollTop } = textAreaRef.current;
-
-      gutterRef.current.scrollTop = scrollTop;
-      errorsRef.current.scrollTop = scrollTop;
-    }
-  }, []);
+  const { dimacs } = useSelector((state: RootState) => state.editor);
 
   return (
     <textarea
       className={styles.textArea}
-      onScroll={handleScroll}
+      onScroll={() => {
+        if (gutterRef.current && textAreaRef.current && errorsRef.current) {
+          const { scrollTop } = textAreaRef.current;
+
+          gutterRef.current.scrollTop = scrollTop;
+          errorsRef.current.scrollTop = scrollTop;
+        }
+      }}
       placeholder="dimacs cnf format allowed here..."
       ref={textAreaRef}
       wrap="off"
