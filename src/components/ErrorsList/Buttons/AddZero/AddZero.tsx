55f3d9f4bfe06acc06b367aec3921c1bb4822cc7
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-29 15:47:16 +0200

    refactoring + now user can edit or delete invalid clauses + edit invalid formula definition

---
+++ src/components/ErrorsList/Buttons/AddZero/AddZero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { toast } from "react-hot-toast";
+
+import { useDispatch } from "react-redux";
+import { addZero } from "../../../../redux/slices/editor";
+
+import { Button } from "@mui/material";
+
+export const AddZero: React.FC<{ line: number }> = ({ line }) => {
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(addZero(line));
+    toast.success("Zero was successfully added!");
+  };
+
+  return (
+    <Button
+      onClick={onClick}
+      sx={{ width: "95px" }}
+      size="small"
+      variant="outlined"
+    >
+      Add zero
+    </Button>
+  );
+};
---
+++ src/components/ErrorsList/Buttons/index.ts
@@ -0,0 +1,3 @@
+export * from "./AddZero/AddZero";
+export * from "./DeleteLine/DeleteLine";
+export * from "./EditLine/EditLine";
