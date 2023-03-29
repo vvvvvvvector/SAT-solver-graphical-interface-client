113931cd6ed2faa9babf837e6e2d5bf204a32454
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-29 14:34:46 +0200

    remove/add/fix error logic in redux editor slice now

--- src/redux/slices/editor.ts
+++ src/redux/slices/editor.ts
@@ -16,25 +16,87 @@ export const editorSlice = createSlice({
   reducers: {
     setDimacs(state, action: PayloadAction<string>) {
       state.dimacs = action.payload;
-      state.errors = [];
     },
     clearDimacs(state) {
       state.dimacs = "";
       state.errors = [];
     },
     addError(state, action: PayloadAction<ErrorType>) {
-      state.errors.push(action.payload);
+      const error = state.errors.find((e) => e.line === action.payload.line);
+
+      if (!error) {
+        state.errors.push(action.payload);
+      } else {
+        state.errors = state.errors.map((error) => {
+          if (error.line === action.payload.line) {
+            return action.payload;
+          }
+          return error;
+        });
+      }
     },
     removeError(state, action: PayloadAction<number>) {
-      state.errors = state.errors.filter((e) => e.line !== action.payload + 1);
+      const error = state.errors.find((e) => e.line === action.payload);
+
+      if (error) {
+        state.errors = state.errors.filter((e) => e.line !== action.payload);
+      }
     },
     clearErrors(state) {
       state.errors = [];
     },
+    addZero(state, action: PayloadAction<number>) {
+      const lines = state.dimacs.split("\n");
+
+      const fixedLines = lines.map((line, index) => {
+        if (index === action.payload - 1) {
+          return line + " 0";
+        }
+
+        return line;
+      });
+
+      state.errors = state.errors.filter((e) => e.line !== action.payload);
+
+      state.dimacs = fixedLines.join("\n");
+    },
+    deleteLine(state, action: PayloadAction<number>) {
+      const lines = state.dimacs.split("\n");
+
+      let definition = lines[0];
+
+      let fixedLines = lines.slice(1).filter((line, index) => {
+        if (index === action.payload - 2) {
+          if (line) {
+            let temp = definition.split(" ").filter((el) => el !== "");
+
+            temp[3] = (parseInt(temp[3]) - 1).toString();
+
+            definition = temp.join(" ");
+          }
+
+          return false;
+        }
+        return true;
+      });
+
+      state.errors = state.errors.filter((e) => e.line !== action.payload);
+
+      fixedLines.unshift(definition);
+
+      state.dimacs = fixedLines.join("\n");
+    },
   },
 });
 
-export const { setDimacs, clearDimacs, addError, removeError, clearErrors } =
-  editorSlice.actions;
+export const {
+  setDimacs,
+  clearDimacs,
+  addError,
+  removeError,
+  clearErrors,
+  addZero,
+  deleteLine,
+} = editorSlice.actions;
 
 export default editorSlice.reducer;
