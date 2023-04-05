948bd9ec6d1dbf5e10602237a71106b3faf14ffb
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-06 01:38:48 +0200

    FindAll refactoring + pagination for solutions component

---
+++ src/components/Panel/Buttons/FindAll/Overlap/Overlap.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "@mui/material/Button";
+
+import styles from "./Overlap.module.scss";
+
+import Status from "../status";
+
+const Overlap: React.FC<{
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  counter: number;
+  loop: React.MutableRefObject<Status>;
+}> = ({ open, setOpen, counter, loop }) => {
+  return (
+    <Backdrop
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+    >
+      <div className={styles.dialog}>
+        <h3>{`Found ${counter} ${
+          counter == 1 ? "solution" : "solutions"
+        }...`}</h3>
+        <CircularProgress color="primary" />
+        <Button
+          sx={{
+            maxWidth: "200px",
+            width: "100%",
+          }}
+          color="primary"
+          variant="outlined"
+          onClick={() => {
+            setOpen(false);
+            loop.current = Status.STOPPED;
+          }}
+        >
+          Stop
+        </Button>
+      </div>
+    </Backdrop>
+  );
+};
+
+export default Overlap;
--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -9,13 +9,22 @@ import NoSolutions from "./NoSolutions/NoSolutions";
 import SolutionsHeader from "./SolutionsHeader/SolutionsHeader";
 
 import styles from "./Solutions.module.scss";
+import { Pagination } from "@mui/material";
 
 export const Solutions: React.FC = () => {
   const containerRef = React.useRef<HTMLDivElement>(null);
 
+  const solutionsPerPage = 10;
+
+  const [page, setPage] = React.useState(0);
+
   const { solutions, opened } = useSelector(
     (state: RootState) => state.solutions
   );
+
+  React.useEffect(() => {
+    setPage(0);
+  }, [solutions]);
 
   // React.useEffect(() => {
   //   containerRef.current?.scrollIntoView({
@@ -60,15 +69,33 @@ export const Solutions: React.FC = () => {
       {opened && (
         <>
           {solutions.length > 0 ? (
-            <div ref={containerRef} className={styles.solutionsContainer}>
-              {solutions.map((solution, solutionIndex) => (
-                <Solution
-                  key={solutionIndex}
-                  solution={solution}
-                  onClickSolution={() => handleSaveSolution(solutionIndex)}
+            <>
+              <div ref={containerRef} className={styles.solutionsContainer}>
+                {solutions
+                  .slice(page, page + solutionsPerPage)
+                  .map((solution, solutionIndex) => (
+                    <Solution
+                      key={solutionIndex}
+                      solution={solution}
+                      onClickSolution={() => handleSaveSolution(solutionIndex)}
+                    />
+                  ))}
+              </div>
+              {solutions.length > solutionsPerPage && (
+                <Pagination
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                  color="primary"
+                  page={page / solutionsPerPage + 1}
+                  onChange={(_, value: number) => {
+                    setPage((value - 1) * solutionsPerPage);
+                  }}
+                  count={Math.ceil(solutions.length / solutionsPerPage)}
                 />
-              ))}
-            </div>
+              )}
+            </>
           ) : (
             <NoSolutions />
           )}
