55414cad6461c2ae1b58eb8d7b3549dc9a9e6538
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-06 01:38:48 +0200

    FindAll refactoring + pagination for solutions component

--- src/components/Panel/Buttons/FindAll/FindAll.tsx
+++ src/components/Panel/Buttons/FindAll/FindAll.tsx
@@ -7,26 +7,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../../../redux/store";
 
 import { Button } from "@mui/material";
-
 import { buttonStyle } from "../../../../shared/mui";
 import { setFormula } from "../../../../redux/slices/formula";
 import {
   clearSolutions,
   setSolutions,
 } from "../../../../redux/slices/solutions";
-
-import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
 import LoopIcon from "@mui/icons-material/Loop";
 
-import styles from "./FindAll.module.scss";
+import Overlap from "./Overlap/Overlap";
 
-enum Status {
-  PENDING = "pending",
-  STOPPED = "stopped",
-  NOTSTARTED = "not started",
-  END = "end",
-}
+import Status from "./status";
 
 export const FindAll: React.FC<{ solver: string }> = ({ solver }) => {
   const dispatch = useDispatch();
@@ -34,35 +25,97 @@ export const FindAll: React.FC<{ solver: string }> = ({ solver }) => {
   const loop = React.useRef<Status>(Status.NOTSTARTED);
 
   const [counter, setCounter] = React.useState(0);
+  const [loading, setLoading] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   const { clauses, changed } = useSelector((state: RootState) => state.formula);
   const { dimacs, errors } = useSelector((state: RootState) => state.editor);
 
-  const [loading, setLoading] = React.useState(false);
-
-  const [open, setOpen] = React.useState(false);
+  React.useEffect(() => {
+    loop.current = Status.NOTSTARTED;
+  }, [dimacs]);
 
   const onClickFindAll = async () => {
-    const solutions: number[][] = [];
+    try {
+      setLoading(true);
+      setOpen(true);
 
-    setLoading(true);
+      const solutions: number[][] = [];
 
-    setOpen(true);
+      const solveResponse = await axiosInstance.post("/solve", {
+        dimacs,
+        solver,
+      });
 
-    const response = await axiosInstance.post("/solve", {
-      dimacs,
-      solver,
-    });
+      if (solveResponse.data.satisfiable) {
+        dispatch(clearSolutions());
 
-    if (response.data.satisfiable) {
-      dispatch(setFormula(response.data.clauses.slice(0, -1)));
+        dispatch(setFormula(solveResponse.data.clauses.slice(0, -1)));
 
-      sessionStorage.setItem("formula", JSON.stringify(response.data.clauses));
+        sessionStorage.setItem(
+          "formula",
+          JSON.stringify(solveResponse.data.clauses)
+        );
 
-      dispatch(clearSolutions());
-      //dispatch(setSolution(response.data.first_solution));
-      solutions.push(response.data.first_solution);
-      setCounter((prev) => prev + 1);
+        solutions.push(solveResponse.data.first_solution);
+        setCounter((prev) => prev + 1);
+
+        loop.current = Status.PENDING;
+
+        while (loop.current === Status.PENDING) {
+          const nextResponse = await axiosInstance.post("/next-solution", {
+            solver,
+            formula: sessionStorage.getItem("formula"),
+          });
+
+          if (nextResponse.data.satisfiable) {
+            solutions.push(nextResponse.data.next_solution);
+            setCounter((prev) => prev + 1);
+
+            sessionStorage.setItem(
+              "formula",
+              JSON.stringify(nextResponse.data.clauses)
+            );
+          } else {
+            loop.current = Status.END;
+          }
+        }
+      }
+
+      setLoading(false);
+      setOpen(false);
+
+      if (loop.current === Status.STOPPED) {
+        toast.success(
+          `Successfully found: ${solutions.length} ${
+            solutions.length === 1 ? "solution" : "solutions"
+          }`,
+          {
+            duration: 3000,
+          }
+        );
+      } else if (loop.current === Status.END) {
+        toast.success("Successfully found all solutions!", {
+          duration: 3000,
+        });
+      }
+
+      setCounter(0);
+
+      dispatch(setSolutions(solutions));
+    } catch (error: any) {
+      console.log(error);
+
+      toast.error("Something went wrong while clicking Find All");
+    }
+  };
+
+  const onClickFindOther = async () => {
+    try {
+      setLoading(true);
+      setOpen(true);
+
+      const solutions: number[][] = [];
 
       loop.current = Status.PENDING;
 
@@ -73,7 +126,6 @@ export const FindAll: React.FC<{ solver: string }> = ({ solver }) => {
         });
 
         if (response.data.satisfiable) {
-          //dispatch(setSolution(response.data.next_solution));
           solutions.push(response.data.next_solution);
           setCounter((prev) => prev + 1);
 
@@ -85,169 +137,62 @@ export const FindAll: React.FC<{ solver: string }> = ({ solver }) => {
           loop.current = Status.END;
         }
       }
-    }
 
-    setLoading(false);
+      setLoading(false);
+      setOpen(false);
 
-    setOpen(false);
-
-    if (loop.current === Status.STOPPED) {
-      setCounter(solutions.length);
-
-      toast.success(
-        `Successfully found: ${solutions.length} ${
-          solutions.length === 1 ? "solution" : "solutions"
-        }`,
-        {
+      if (loop.current === Status.END) {
+        toast.success("Successfully found all other solutions!", {
           duration: 3000,
-        }
-      );
-    } else if (loop.current === Status.END) {
-      toast.success("Successfully found all solutions!", {
-        duration: 3000,
-      });
-    }
-
-    setCounter(0);
-
-    dispatch(setSolutions(solutions));
-  };
-
-  const onClickFindOther = async () => {
-    const solutions: number[][] = [];
-
-    setOpen(true);
-
-    setLoading(true);
-
-    loop.current = Status.PENDING;
-
-    while (loop.current === Status.PENDING) {
-      const response = await axiosInstance.post("/next-solution", {
-        solver,
-        formula: sessionStorage.getItem("formula"),
-      });
-
-      if (response.data.satisfiable) {
-        //dispatch(setSolution(response.data.next_solution));
-        solutions.push(response.data.next_solution);
-        setCounter((prev) => prev + 1);
-
-        sessionStorage.setItem(
-          "formula",
-          JSON.stringify(response.data.clauses)
-        );
+        });
       } else {
-        loop.current = Status.END;
+        setCounter(solutions.length);
+
+        toast.success(
+          `Successfully found: ${solutions.length} ${
+            solutions.length === 1 ? "solution" : "solutions"
+          }`,
+          {
+            duration: 3000,
+          }
+        );
       }
+
+      setCounter(0);
+
+      dispatch(setSolutions(solutions));
+    } catch (error: any) {
+      console.log(error);
+
+      toast.error("Something went wrong while clicking Find Other");
     }
-
-    setOpen(false);
-
-    setLoading(false);
-
-    if (loop.current === Status.END) {
-      toast.success("Successfully found all other solutions!", {
-        duration: 3000,
-      });
-    } else {
-      setCounter(solutions.length);
-
-      toast.success(
-        `Successfully found: ${solutions.length} ${
-          solutions.length === 1 ? "solution" : "solutions"
-        }`,
-        {
-          duration: 3000,
-        }
-      );
-    }
-
-    dispatch(setSolutions(solutions));
-
-    setCounter(0);
   };
 
   return (
     <>
       {clauses.length > 0 && !changed ? (
-        <>
-          <Button
-            disabled={errors.length > 0 || loading}
-            onClick={onClickFindOther}
-            sx={buttonStyle}
-            variant="contained"
-            endIcon={<LoopIcon />}
-          >
-            Find other solutions
-          </Button>
-          {loading && (
-            <Backdrop
-              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={open}
-            >
-              <div className={styles.dialog}>
-                <h3>{`Found ${counter} ${
-                  counter == 1 ? "solution" : "solutions"
-                }`}</h3>
-                <CircularProgress color="primary" />
-                <Button
-                  sx={{
-                    maxWidth: "200px",
-                    width: "100%",
-                  }}
-                  color="primary"
-                  variant="outlined"
-                  onClick={() => {
-                    setOpen(false);
-                    loop.current = Status.STOPPED;
-                  }}
-                >
-                  Stop
-                </Button>
-              </div>
-            </Backdrop>
-          )}
-        </>
+        <Button
+          disabled={errors.length > 0 || loading}
+          onClick={onClickFindOther}
+          sx={buttonStyle}
+          variant="contained"
+          endIcon={<LoopIcon />}
+        >
+          {loading ? "Loading..." : "Find other solutions"}
+        </Button>
       ) : (
-        <>
-          <Button
-            disabled={dimacs.length === 0 || errors.length > 0}
-            onClick={onClickFindAll}
-            sx={buttonStyle}
-            variant="contained"
-            endIcon={<LoopIcon />}
-          >
-            Find All
-          </Button>
-          {loading && (
-            <Backdrop
-              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={open}
-            >
-              <div className={styles.dialog}>
-                <h3>{`Found ${counter} ${
-                  counter == 1 ? "solution" : "solutions"
-                }`}</h3>
-                <CircularProgress color="primary" />
-                <Button
-                  sx={{
-                    maxWidth: "200px",
-                    width: "100%",
-                  }}
-                  color="primary"
-                  variant="outlined"
-                  onClick={() => {
-                    setOpen(false);
-                    loop.current = Status.STOPPED;
-                  }}
-                >
-                  Stop
-                </Button>
-              </div>
-            </Backdrop>
-          )}
-        </>
+        <Button
+          disabled={dimacs.length === 0 || errors.length > 0}
+          onClick={onClickFindAll}
+          sx={buttonStyle}
+          variant="contained"
+          endIcon={<LoopIcon />}
+        >
+          Find All
+        </Button>
+      )}
+      {loading && (
+        <Overlap loop={loop} counter={counter} setOpen={setOpen} open={open} />
       )}
     </>
   );
---
+++ src/components/Panel/Buttons/FindAll/status.ts
@@ -0,0 +1,8 @@
+enum Status {
+  PENDING = "pending",
+  STOPPED = "stopped",
+  NOTSTARTED = "not started",
+  END = "end",
+}
+
+export default Status;
