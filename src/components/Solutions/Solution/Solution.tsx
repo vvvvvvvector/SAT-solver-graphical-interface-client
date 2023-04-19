9a995e860be6f8367fbf9569c4579d97fde0145e
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-19 13:48:29 +0200

    new save solution button

--- src/components/Solutions/Solution/Solution.tsx
+++ src/components/Solutions/Solution/Solution.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import VariableValue from './VariableValue/VariableValue';
 
+import SaveAsIcon from '@mui/icons-material/SaveAs';
+
 import styles from './Solution.module.scss';
 
 interface SolutionProps {
@@ -11,13 +13,20 @@ interface SolutionProps {
 
 const Solution: FC<SolutionProps> = ({ solution, onClickSolution }) => {
   return (
-    <div
-      onClick={onClickSolution}
-      className={`${styles.solutions} hide-scrollbars`}
-    >
-      {solution.map((value, valueIndex) => (
-        <VariableValue key={valueIndex} value={value} />
-      ))}
+    <div className={styles.container}>
+      <div className={`${styles.solutions} hide-scrollbars`}>
+        {solution.map((value, valueIndex) => (
+          <VariableValue key={valueIndex} value={value} />
+        ))}
+      </div>
+      <button onClick={onClickSolution}>
+        <SaveAsIcon
+          sx={{
+            color: '#d5d5d5',
+            fontSize: '1.2rem',
+          }}
+        />
+      </button>
     </div>
   );
 };
