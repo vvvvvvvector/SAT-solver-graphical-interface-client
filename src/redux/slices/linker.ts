3dc458ce887b5b6396519d027a8715567fd6bab3
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-20 16:08:54 +0200

    hotfix: no formulas filter while linking

--- src/redux/slices/linker.ts
+++ src/redux/slices/linker.ts
@@ -19,31 +19,9 @@ export const linkerSlice = createSlice({
     setLinked(state, action: PayloadAction<string>) {
       state.linked = action.payload;
     },
-    filterFormula(state, action: PayloadAction<1 | 2>) {
-      if (action.payload === 1) {
-        state.first = state.first
-          .split('\n')
-          .filter((line) => {
-            return line.match(
-              /^p\s+cnf\s+[1-9][0-9]*\s+[1-9][0-9]*\s*$|^\s*(?:-?[1-9][0-9]*\s+)+0\s*$/
-            );
-          })
-          .join('\n');
-      } else {
-        state.second = state.second
-          .split('\n')
-          .filter((line) => {
-            return line.match(
-              /^p\s+cnf\s+[1-9][0-9]*\s+[1-9][0-9]*\s*$|^\s*(?:-?[1-9][0-9]*\s+)+0\s*$/
-            );
-          })
-          .join('\n');
-      }
-    },
   },
 });
 
-export const { setFirst, setSecond, setLinked, filterFormula } =
-  linkerSlice.actions;
+export const { setFirst, setSecond, setLinked } = linkerSlice.actions;
 
 export default linkerSlice.reducer;
