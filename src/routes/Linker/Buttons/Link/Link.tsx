4f0ad1309742d3c14f02ec4ccc20b6d1cffd7e2e
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-06-25 16:03:10 +0200

    refactoring

--- src/routes/Linker/Buttons/Link/Link.tsx
+++ src/routes/Linker/Buttons/Link/Link.tsx
@@ -17,6 +17,8 @@ export const Link: FC = () => {
   const [loading, setLoading] = useState(false);
 
   const onClickLink = async () => {
+    const id = toast.loading('Linking...');
+
     try {
       setLoading(true);
 
@@ -29,27 +31,27 @@ export const Link: FC = () => {
 
       dispatch(setLinked(response.data.result));
 
-      toast.success('Formulas were successfully linked!');
+      toast.success('Formulas were successfully linked!', { id });
     } catch (error: any) {
       setLoading(false);
 
       if (error.response.status === 421) {
         // Formulas are the same
-        toast.error(error.response.data.detail);
+        toast.error(error.response.data.detail, { id });
       } else if (error.response.status === 422) {
         // There is no formula definition in the first formula
-        toast.error(error.response.data.detail);
+        toast.error(error.response.data.detail, { id });
       } else if (error.response.status === 423) {
         // There is no formula definition in the second formula
-        toast.error(error.response.data.detail);
+        toast.error(error.response.data.detail, { id });
       } else if (error.response.status === 424) {
         // The first formula has wrong amount of clauses!
-        toast.error(error.response.data.detail);
+        toast.error(error.response.data.detail, { id });
       } else if (error.response.status === 425) {
         // The second formula has wrong amount of clauses!
-        toast.error(error.response.data.detail);
+        toast.error(error.response.data.detail, { id });
       } else {
-        toast.error('Something went wrong!');
+        toast.error('Something went wrong!', { id });
       }
 
       console.error(error);
