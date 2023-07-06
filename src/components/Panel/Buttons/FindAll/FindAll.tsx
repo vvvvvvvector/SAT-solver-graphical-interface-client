a66ed7f3bb456e7c189616c809040033b1f20a2a
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-07-06 02:46:47 +0200

    refactoring

--- src/components/Panel/Buttons/FindAll/FindAll.tsx
+++ src/components/Panel/Buttons/FindAll/FindAll.tsx
@@ -34,7 +34,8 @@ export const FindAll: FC<FindAllProps> = ({ solver }) => {
   const solutionsLength = useAppSelector(
     (state) => state.solutions.solutions.length
   );
-  const isFormulaChanged = useAppSelector((state) => state.formula.changed);
+
+  const cnfFormulaLength = useAppSelector((state) => state.formula.clauses.length);
   const { dimacs, errors } = useAppSelector((state) => state.editor);
 
   useEffect(() => {
@@ -202,7 +203,7 @@ export const FindAll: FC<FindAllProps> = ({ solver }) => {
 
   return (
     <>
-      {solutionsLength > 0 && !isFormulaChanged ? (
+      {solutionsLength > 0 && cnfFormulaLength !== 0 ? (
         <Button
           disabled={errors.length > 0 || loading}
           onClick={onClickFindOther}
